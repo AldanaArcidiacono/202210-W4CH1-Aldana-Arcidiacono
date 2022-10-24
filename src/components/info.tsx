69ea91c8b8a342646cf0a31446ef4d257f9bb2b9
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-24 20:40:46 +0200

    Change info component

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,19 +1,11 @@
+import { Button } from "./button";
+
 export function Info() {
-    //  const initialState: IState = { counter: 0 };
-    //  const [state, setState] = useState(initialState);
-
-    //  useEffect(() => {
-    //      console.log(state);
-    //  }, [state]);
-
-    //  const handlerCounter = (value: number) => {
-    //      setState({ ...state, counter: state.counter + value });
-    //  };
     return (
         <>
             <section className="controls">
                 <p className="info">0 gentlemen pointing at you</p>
-                <button className="button button--select">Select all</button>
+                <Button></Button>
             </section>
         </>
     );
